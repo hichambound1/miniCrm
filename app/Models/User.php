fa743e760071bdde0d356825273ec34f9d2d0564
Hicham.BOUNDOUQ <hicham.boundouq@um6p.ma>
2023-04-24 18:46:32 +0100

    add entreprise_id to users

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,7 +26,8 @@ class User extends Authenticatable
         'birthday',
         'tel',
         'address',
-        'status_id'
+        'status_id',
+        'entreprise_id'
     ];
 
     /**
@@ -47,4 +48,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function entreprise()
+    {
+        return $this->belongsTo(Entreprise::class);
+    }
 }
