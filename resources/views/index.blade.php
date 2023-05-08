35b5d67794142fa37e3a5c6b4aebe9b8d33311dd
Hicham.BOUNDOUQ <hicham.boundouq@um6p.ma>
2023-05-08 23:28:59 +0100

    wip

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends("layout")
+@section("content")
+
+
+<div class="row">
+    <div class="col-12">
+        <div class="page-title-box d-sm-flex align-items-center justify-content-between">
+            <h4 class="mb-sm-0">Starter page</h4>
+
+            
+
+        </div>
+    </div>
+</div>
+@endsection
